a42ceb0df70aa4937958f8472a9ec952c2abd3ef
Erick Hitter <git-contrib@ethitter.com>
2021-01-07 15:31:11 -0800

    PHPCS: dynamically set `installed_paths`

--- phpcs/provision.sh
+++ phpcs/provision.sh
@@ -9,8 +9,12 @@ vvv_info " * Install PMC Codesniffer..."
 git clone git@bitbucket.org:penskemediacorp/pmc-codesniffer.git "$STANDARDS_DIR"
 noroot composer update --no-ansi --no-autoloader --no-progress -d "$STANDARDS_DIR"
 
-vvv_info " * Setting PmcWpVip as default PHPCS standard..."
-phpcs --config-set installed_paths ./CodeSniffer/Standards/PmcWpVip/,./CodeSniffer/Standards/PmcLaravel/,./CodeSniffer/Standards/vendor/wp-coding-standards/wpcs/,./CodeSniffer/Standards/vendor/automattic/vipwpcs/,./CodeSniffer/Standards/vendor/sirbrillig/phpcs-variable-analysis/
-phpcs --config-set default_standard PmcWpVip
+vvv_info " * Setting PmcWpVipGo as default PHPCS standard..."
+
+PMCCS_STANDARDS=$(find $STANDARDS_DIR -maxdepth 1 -type d -name "Pmc*" | sed -e 's,/srv/www/phpcs,.,' | sed -e :a -e N -e 's/\n/,/' -e ta)
+PMCCS_DEPENDENCIES=$(find "${STANDARDS_DIR}vendor/" -maxdepth 3 -type f -name composer.json -not -path "${STANDARDS_DIR}vendor/dealerdirect/*" -not -path "${STANDARDS_DIR}vendor/squizlabs/*" | sed -e 's,/srv/www/phpcs,.,' | sed -e 's/composer.json$//' | sed -e :a -e N -e 's/\n/,/' -e ta)
+
+phpcs --config-set installed_paths "${PMCCS_STANDARDS},${PMCCS_DEPENDENCIES}"
+phpcs --config-set default_standard PmcWpVipGo
 phpcs -i
 phpcs --config-show
