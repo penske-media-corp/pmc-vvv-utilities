004d05889c41d20f9651fa5683918838e024a29f
SohamPatel46 <soham.patel@rtcamp.com>
2023-12-07 14:02:21 +0530

    Remove sudo command

--- bin/setup-nova-brands.sh
+++ bin/setup-nova-brands.sh
@@ -6,8 +6,8 @@
 #
 # Description:
 #   This script installs all nova themes, so that you can set up all nova themes under
-#   single site installation. You can run this script from any sites theme folder, you would
-#   like to install all themes.
+#   single site installation. You can run this script from inside vagrant shh under any sites theme folder.
+#
 #   You must also make it executable by running the command:
 #     chmod +x setup-nova-brands.sh
 #
@@ -16,16 +16,17 @@
 #    to artforum theme folder and run the script with its location :
 #
 #    cd /srv/www/artforum-com/public_html/wp-content/themes
-#    /srv/provision/extensions/pmc/bin/setup-nova-brands.sh
+#    ./srv/provision/extensions/pmc/bin/setup-nova-brands.sh
 #
 # Created: 7 Sept 2023
 ################################################################################
 
 # Clone all nova brands
-sudo git clone git@github.com:penske-media-corp/pmc-artforum-2023.git
-sudo git clone git@github.com:penske-media-corp/pmc-indiewire-2023.git
-sudo git clone git@github.com:penske-media-corp/pmc-nova-theme.git
-sudo git clone git@github.com:penske-media-corp/pmc-stylecaster-2023.git
-sudo git clone git@github.com:penske-media-corp/pmc-tvline-2023.git
+git clone git@github.com:penske-media-corp/pmc-artforum-2023.git
+git clone git@github.com:penske-media-corp/pmc-indiewire-2023.git
+git clone git@github.com:penske-media-corp/pmc-nova-theme.git
+git clone git@github.com:penske-media-corp/pmc-rr-giftguide-2023.git
+git clone git@github.com:penske-media-corp/pmc-stylecaster-2023.git
+git clone git@github.com:penske-media-corp/pmc-tvline-2023.git
 
 echo "All themes downloaded !!"
